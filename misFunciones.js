16e7bd7202790a6fac9db71f8709cfba949f8bf2
baltasarlopezv <baltasarlopezv@gmail.com>
2023-07-04 23:51:39 -0300

    Se agregaron funciones de conversion de dolar

--- misFunciones.js
+++ misFunciones.js
@@ -6,48 +6,93 @@
  */
 
 function convertirBlue() {
-    var valor = parseFloat(document.getElementById("cantidad").value);
-    var resultado = valor * 472;
-    alert("El cambio de dólares a pesos es: $" + resultado);
+    var valorDolares = parseFloat(document.getElementById("cantidad").value);
+    var valorPesos = valorDolares * 472;
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "black";
+    context.font = "22px Arial";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    context.fillText("Valor en pesos: $" + valorPesos.toFixed(2), x, y);
 }
 
 function convertirBolsa() {
-    var valor = parseFloat(document.getElementById("cantidad").value);
-    var resultado = valor * 432;
-    alert("El cambio de dólares a pesos es: $" + resultado);
+    var valorDolares = parseFloat(document.getElementById("cantidad").value);
+    var valorPesos = valorDolares * 432;
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "black";
+    context.font = "22px Arial";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    context.fillText("Valor en pesos: $" + valorPesos.toFixed(2), x, y);
 }
 
 function convertirContado() {
-    var valor = parseFloat(document.getElementById("cantidad").value);
-    var resultado = valor * 438;
-    alert("El cambio de dólares a pesos es: $" + resultado);
-}
+    var valorDolares = parseFloat(document.getElementById("cantidad").value);
+    var valorPesos = valorDolares * 438;
 
-function convertirCripto() {
-    var valor = parseFloat(document.getElementById("cantidad").value);
-    var resultado = valor * 441;
-    alert("El cambio de dólares a pesos es: $" + resultado);
-}
-
-function convertirSolidario() {
-    var valor = parseFloat(document.getElementById("cantidad").value);
-    var resultado = valor * 390;
-    alert("El cambio de dólares a pesos es: $" + resultado);
-}
-
-/**
- * Dibuja el símbolo del dolar "$" en el canvas.
- * @param (string) id - El id del canvas donde se dibujará el símbolo del dolar.
- * @param (string) color - El color del símbolo del dolar.
- * @returns (void)
- */
-function dibujarSimboloDolar(canvasId, color) {
-    var canvas = document.getElementById(canvasId);
+    var canvas = document.getElementById("myCanvas");
     var context = canvas.getContext("2d");
 
-    context.font = "100px Arial";
-    context.fillStyle = color;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "black";
+    context.font = "22px Arial";
     context.textAlign = "center";
     context.textBaseline = "middle";
-    context.fillText("$", canvas.width / 2, canvas.height / 2);
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    context.fillText("Valor en pesos: $" + valorPesos.toFixed(2), x, y);
+}
+
+
+function convertirCripto() {
+    var valorDolares = parseFloat(document.getElementById("cantidad").value);
+    var valorPesos = valorDolares * 441;
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "black";
+    context.font = "22px Arial";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    context.fillText("Valor en pesos: $" + valorPesos.toFixed(2), x, y);
+}
+
+
+function convertirSolidario() {
+    var valorDolares = parseFloat(document.getElementById("cantidad").value);
+    var valorPesos = valorDolares * 390;
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = "black";
+    context.font = "22px Arial";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    context.fillText("Valor en pesos: $" + valorPesos.toFixed(2), x, y);
 }
